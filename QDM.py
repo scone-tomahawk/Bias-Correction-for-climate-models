4992c46158b4bf1c1a54cbe478bd950dc488e317
elleesse <78006947+scone-tomahawk@users.noreply.github.com>
2021-04-18 01:05:01 +0800

    Update QDM.py

--- QDM.py
+++ QDM.py
@@ -7,29 +7,27 @@ import rpy2.robjects as rObj
 
 pandas2ri.activate()
 
-def bcQDM(obs_h,mod_h,mod_p,ratio=True,subs=rObj.NULL,ntau=rObj.NULL):
-    _dat = rMBC.QDM(obs_h,mod_h,mod_p,ratio=ratio,subsample=subs,n_tau=ntau)
-    return (_dat[0],_dat[1])
+def bcQDM(obs_dat,mod_dat,mod_nf,ratio=True,subs=rObj.NULL,ntau=rObj.NULL):
+    _dat = rMBC.QDM(obs_dat,mod_dat,mod_nf,ratio=ratio,subsample=subs,n_tau=ntau)
+    return _dat[0],_dat[1]
 
-def applyQDM(obs_h,mod_h,mod_p,**kwargs):
+def apply_QDM(obs_dat,mod_dat,mod_nf,**kwargs):
     start = time.time()
    
-    mod_h,mod_p,var = mod_h.rename({'time':'t1'}),mod_p.rename({'time':'t2'}),list(obs_h.keys())
-    time1,time2 = mod_h['t1'].values,mod_p['t2'].values
-    mod_h,mod_p = mod_h.dropna('t1'),mod_p.dropna('t2')
-     
-    hist,proj = xr.apply_ufunc(bcQDM,obs_h,mod_h,mod_p,kwargs=kwargs,
-                            input_core_dims=[['time'],['t1'],['t2']],
-                            output_core_dims=[['t1'],['t2']],vectorize=True,dask='allowed',
-                            output_dtypes=[np.float,np.float])
+    mod_dat,mod_nf,var = mod_dat.rename({'time':'t1'}),mod_nf.rename({'time':'t2'}),list(obs_dat.keys())
+    time1,time2 = mod_dat['t1'].values,mod_nf['t2'].values
+    mod_dat,mod_nf = mod_dat.dropna('t1'),mod_nf.dropna('t2')
+    tRF,tPR = len(mod_dat['t1']),len(mod_nf['t2'])
     
-    if (obs_h[var[0]].ndim == 1):
-        oc = hist.assign_coords({'t1':time1}).rename({'t1':'time'})
-        pc = proj.assign_coords({'t2':time2}).rename({'t2':'time'})
+    hist,proj=xr.apply_ufunc(bcQDM,obs_dat,mod_dat,mod_nf,kwargs=kwargs,input_core_dims=[['time'],['t1'],['t2']],
+                      output_core_dims=[['t1'],['t2']],vectorize=True,dask='allowed')
+    
+    if (obs_dat[var[0]].ndim == 1):
+        oc = hist.rename({'t1':'time'}).reindex({'time':time1})
+        pc = proj.rename({'t2':'time'}).reindex({'time':time2})
     else:
-        oc = hist.assign_coords({'t1':time1}).transpose('t1','lat','lon').rename({'t1':'time'})
-        pc = proj.assign_coords({'t2':time2}).transpose('t2','lat','lon').rename({'t2':'time'})
-    
+        oc = hist.rename({'t1':'time'}).reindex({'time':time1}).transpose('time','lat','lon')
+        pc = proj.rename({'t2':'time'}).reindex({'time':time2}).transpose('time','lat','lon')
     end = time.time()
     print("▮▮▮ Elapsed time in real time :" , time.strftime("%M:%S",time.gmtime(end-start)),"minutes ▮▮▮")
     return oc,pc
